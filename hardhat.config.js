e892da7deefcf261d20ceb6eca85833f99c45ad4
ali <a_icam@yahoo.com>
2022-06-30 11:10:30 +0430

    Marketplace.sol

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require('dotenv').config();
 
-const { PRIVATE_KEY, NFT_URL } = process.env;
+const { PRIVATE_KEY, PRIVATE_KEY2, NFT_URL } = process.env;
 
 module.exports = {
   solidity: "0.8.4",
@@ -10,7 +10,7 @@ module.exports = {
     hardhat: {},
     polygon_mumbai: {
       url: NFT_URL,
-      accounts: [`0x${PRIVATE_KEY}`]
+      accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY2}`]
     }
   },
   paths: {
@@ -18,5 +18,8 @@ module.exports = {
     sources: "./src/backend/contracts",
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
+  },
+  mocha: {
+    timeout: 100000000
   },
 };
--- src/backend/test/NFTMarketplace.test.js
+++ src/backend/test/NFTMarketplace.test.js
@@ -1,33 +1,61 @@
 const {expect} = require('chai');
+const toWei = (num) => ethers.utils.parseEther(num.toString())
+const fromWei = (num) => ethers.utils.formatEther(num)
 
 
 describe("NFTMarketplace", () => {
-    let deployer, marketplace, nft;
-    const addr2 = "0xf0cc965724304676e37d775f08f4e96bb12052cd776c34008f7bed44dcbd3bc7";
+    let deployer, marketplace, nft, addr2;
     beforeEach(async () => {
         const Marketplace = await ethers.getContractFactory("Marketplace");
         const NFT = await ethers.getContractFactory("NFT");
 
-        [deployer] = await ethers.getSigners();
+        [deployer, addr2] = await ethers.getSigners();
         marketplace = await Marketplace.deploy(1);
         nft = await NFT.deploy();
     });
     describe("Deployment ", () => {
+        it("Track NFT name & symbol", async () => {
+            expect(await nft.name()).to.equal("DApp NFT");
+            expect(await nft.symbol()).to.equal("DAPP")
+        });
         it("Track feePercent and feeAccount ", async () => {
             expect(await marketplace.feeAccount()).to.equal(deployer.address);
             expect(await marketplace.feePercent()).to.equal(1);
-        })
+        });
     });
-    // describe("Minting NFT", () => {
-    //     let URI="Sample URI";
-    //     it("Track minting process", async () => {
-    //         await nft.connect(addr2).mint(URI);
-    //         console.log(nft);
-    //         expect(await nft.tokenCount()).to.equal(1);
-    //     })
-    // })
-    describe("Making marketplace item", () => {
 
+    let URI="Sample URI";
+    describe("Minting NFT", () => {
+        it("Track minting process", async () => {
+            await nft.connect(addr2).mint(URI);
+            console.log(addr2.address);
+            expect(await nft.tokenCount()).to.equal(1);
+            expect(await nft.balanceOf(addr2.address)).to.equal(1);
+            expect(await nft.tokenURI(1)).to.equal(URI);
+        })
+    })
+    describe("Making marketplace item", () => {
+        beforeEach(async () => {
+            await nft.connect(addr2).mint(URI);
+            await nft.connect(addr2).setApprovalForAll(marketplace.address, true);
+        });
+        it("Track newly created item, transfer NFT from seller marketplace and emit Offered event", async () => {
+            await expect(marketplace.connect(addr2).makeItem(nft.address, 1, toWei(1)))
+                .to.emit(marketplace, "Offered")
+                .withArgs(
+                    1,
+                    nft.address,
+                    1,
+                    toWei(1),
+                    addr2.address
+                )
+            // New Owner
+            expect(await nft.ownerOf(1)).to.equal(marketplace.address);
+            // Prev Owner
+            expect(await nft.ownerOf(0)).to.equal(addr2.address);
+            // Check item
+
+        });
     })
 
 })
